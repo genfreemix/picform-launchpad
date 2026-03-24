699f07d59a277412a2394ce72cbd04f26f7b4a8c
Gen Freemix <genfreemix@gmail.com>
2026-03-24 22:32:51 +1100

    Hero: убран scroll indicator (прыгающая мышка)

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -88,13 +88,6 @@ const HeroSection = () => {
           ))}
         </div>
       </div>
-
-      {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
-        <div className="w-6 h-10 border-2 border-foreground/30 rounded-full flex justify-center pt-2">
-          <div className="w-1 h-2 bg-primary rounded-full" />
-        </div>
-      </div>
     </section>
   );
 };
